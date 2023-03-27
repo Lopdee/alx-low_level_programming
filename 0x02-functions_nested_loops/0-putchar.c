b7039069fe65a7862d1cdf17b4f64f181b46ffde
Lopdee <ugochukwulopezubani@gmail.com>
2023-03-27 04:34:28 -0700

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
-1#include "main.h"
-2/**
-3* main - print _putchar
-4* is written by ebah
-5* Return:0
-6*/
-7
-8int main(void)
-9{
-_putchar(' ');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h)
-_putchar('a');
-_putchar('r');
-_putchar('\n');
- return (0);
+#include "main.h"
+
+/**
+* main - prints putchar
+* Return: always 0
+*/
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
